4b599a711b9c98137d29a7c79a353e76c3a3fdd2
abonis-solutec <alexis.bonis@consultants-solutec.fr>
2024-10-17 10:09:39 +0200

    Add basic class

---
+++ base.java
@@ -0,0 +1,30 @@
+public class formulaire {
+    private:
+        String nom;
+        String date;
+
+    public:
+        String getNom() {
+            return nom;
+        }
+        String getDate() {
+            return date;
+        }
+}
+
+public class formulaire_dentiste extends formulaire {
+    private:
+        String nom_dentiste;
+    public:
+        String getNomD() {
+            return nom_dentiste;
+        }
+}
+
+public static void main(String[] args) {
+    formulaire f, f2, f3;
+    formulaire_dentiste fd;
+    String nom = f.getNom();
+    String nompatient = fd.getNom();
+    String nomdentiste = fd.getNomD();
+}
