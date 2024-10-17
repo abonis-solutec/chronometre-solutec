88cf72a005e2b55d6ca9d59e79a81ad34c6361fd
abonis-solutec <alexis.bonis@consultants-solutec.fr>
2024-10-17 15:10:02 +0200

    Refactoring App.java
    minor fix

--- ChronoApp.java
+++ ChronoApp.java
@@ -1,4 +1,4 @@
-public class App {
+public class ChronoApp {
     public static void main(String[] args) {
         formulaire_dentiste fd = new formulaire_dentiste();
         fd.setNom("Le patient");
@@ -7,5 +7,7 @@ public class App {
         String nomdentiste = fd.getNomD();
         System.out.println("\nNom du dentiste : " + nomdentiste);
         System.out.println("Nom du patient : " + nompatient);
+        //long time_ms = System.currentTimeMillis();
+        //System.out.println("Temps actuel : " + time_ms);
     }
 }
