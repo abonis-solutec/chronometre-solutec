7701a3fc5628c0ab3c802921a9676c1f154ca732
abonis-solutec <alexis.bonis@consultants-solutec.fr>
2024-10-18 09:47:10 +0200

    Update ChronoAppX.java
    Ajout d'un bouton pour start

--- ChronoAppX.java
+++ ChronoAppX.java
@@ -1,19 +1,46 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Action;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class ChronoAppX {
+    private static boolean running = false;
     public static void main(String[] args) {
+        JFrame frame = new JFrame("Super Chrono Solutec3000");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        JPanel panel = new JPanel();
+        JButton startButton = new JButton("Start");
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                running = true;
+            }
+        });
+        panel.add(startButton);
+        frame.add(panel);
+        frame.setVisible(true);
+
         Chrono ch = new Chrono();
         long starting_time_ = ch.getStartingTime();
         long secondes_ = ch.getSecondes();
         long minutes_ = ch.getMinutes();
-        boolean running = true;
         long[] tab_ = new long[3];
         tab_[0] = starting_time_;
         tab_[1] = secondes_;
         tab_[2] = minutes_;
+
+        System.out.println("0 : 0");
         while (tab_[2] < 90) {
             if(running) {
                 tab_ = ch.unit(tab_[0], tab_[1], tab_[2]);
                 System.out.println(tab_[2]+" : "+tab_[1]);
             }
+            else
+                System.out.println("0 : 0");
         }
         /*long starting_time = System.currentTimeMillis();
         //System.out.println("Temps actuel : " + time_ms);
